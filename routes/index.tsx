a36fe80709663dec600a90728dc4231fe6610ec0
josh <joshuaholman5@gmail.com>
2025-04-20 23:11:24 -0700

    button links look better

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,25 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { Button } from "../components/Button.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
+  const buttonClasses =
+    "bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 rounded";
   return (
     <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-      <Counter count={count} />
+      <div class="p-4">
+        <h1 class="text-xl font-bold text-blue-600">Hello! I'm Josh Holman.</h1>
+      </div>
+
+      <div class="p-4">
+        <a href="/portfolio" class={buttonClasses}>Portfolio</a>
+      </div>
+
+      <div class="p-4">
+        <a href="/resume" class={buttonClasses}>Resume</a>
+      </div>
+
+      <div class="p-4">
+        <a href="/tutoring" class={buttonClasses}>Tutoring</a>
+      </div>
     </div>
   );
 }
