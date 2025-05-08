94a2e7880805556cffc3b601847feb250a6eb761
josh <joshuaholman5@gmail.com>
2025-05-07 20:09:17 -0700

    sadf

--- routes/portfolio.tsx
+++ routes/portfolio.tsx
@@ -1,5 +1,59 @@
 export default function PortfolioPage() {
   return (
-    <p>This is the porfolio page</p>
+    <>
+      <section class="intro">
+        <p>
+          Hi there. I am a Computer Science student at CSU, Fullerton (Aug 2023
+          - Dec 2025 expected). I also work part-time as a math tutor.
+        </p>
+        <a href="https://www.github.com/TheJolman" class="profile-link">
+          My GitHub.
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joshua-holman-685a39244/"
+          class="profile-link"
+        >
+          My Linkedin.
+        </a>
+        <a
+          href="https://github.com/TheJolman/resume/blob/main/resume.pdf"
+          class="profile-link"
+        >
+          My Resume (PDF).
+        </a>
+      </section>
+
+      <section class="container mx-auto items-center">
+        <h3 class="section-title">Relevant Coursework</h3>
+        <ul class="class-list">
+          <li>Artifical Intelligence</li>
+          <li>Machine Learning</li>
+          <li>Data Science</li>
+          <li>Database Systems</li>
+          <li>Operating Systems</li>
+        </ul>
+      </section>
+
+      <section class="interests">
+        <h3 class="section-title">My Technical Interests</h3>
+        <ul class="interests-list">
+          <li>Open Source Software Development</li>
+          <li>Compilers</li>
+          <li>Systems Programming</li>
+          <li>Data Science and ML</li>
+          <li>Mathematics</li>
+        </ul>
+      </section>
+
+      <section class="skills">
+        <h3 class="section-title">Technical Skills</h3>
+        <ul class="skills-list">
+          <li>Python, C/C++, Java, Go, Lua, bash, SQL, Matlab</li>
+          <li>Django, FastAPI, Python DS/ML libraries</li>
+          <li>Nix/NixOS, Linux, Docker</li>
+          <li>GitHub Actions, Vim</li>
+        </ul>
+      </section>
+    </>
   );
 }
