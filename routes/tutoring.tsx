0efb8b4e0fb228e2d5e32b47a9e68d5c068cfce7
Josh Holman <josh@jolman.me>
2025-07-08 13:22:28 -0700

    update tutoring page (#7)

--- routes/tutoring.tsx
+++ routes/tutoring.tsx
@@ -1,7 +1,27 @@
 export default function TutoringPage() {
   return (
-    <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-      <p>Tutoring page</p>
-    </div>
+    <>
+      <div className="max-w-screen-md mx-auto mt-5 text-center">
+        <h1 className="my-5 font-bold text-xl">Tutoring</h1>
+        <p className="inline">
+          I do private math tutoring upon request. For availability, please
+          reach out to me at&nbsp;
+        </p>
+        <a href="mailto:josh@jolman.me" className="text-blue-500 inline">
+          josh@jolman.me
+        </a>.
+        <br />
+        <h2 className="my-3 font-bold text-lg">Rates</h2>
+        <p>
+          My current rate is $25 for sessions not exceeding 1 hour, and $25/hour
+          thereafter.
+        </p>
+        <h2 className="my-3 font-bold text-lg">Topics Offered</h2>
+        <p>
+          I tutor all topics found in K-12 math. For pre-calculus and higher, I
+          require notice ahead of time so I can prepare.
+        </p>
+      </div>
+    </>
   );
 }
