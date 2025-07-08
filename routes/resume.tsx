30429d0c5bce81278088cf6d78275bf87e42c0df
Josh Holman <josh@jolman.me>
2025-07-07 21:14:02 -0700

    improve resume links

--- routes/resume.tsx
+++ routes/resume.tsx
@@ -1,11 +1,12 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { CSS, render } from "@deno/gfm"
+import { CSS, render } from "@deno/gfm";
 import { Head } from "$fresh/runtime.ts";
+import { PortfolioLinks } from "../components/PortfolioLinks.tsx";
 
 export const handler: Handlers<string> = {
   async GET(_, ctx) {
     const resp = await fetch(
-      "https://raw.githubusercontent.com/TheJolman/resume/refs/heads/main/resume.md"
+      "https://raw.githubusercontent.com/TheJolman/resume/refs/heads/main/resume.md",
     );
     if (!resp.ok) {
       return new Response("Could not find resume.", { status: 404 });
@@ -13,8 +14,15 @@ export const handler: Handlers<string> = {
     const markdown = await resp.text();
     const html = render(markdown);
     return ctx.render(html);
-  }
-}
+  },
+};
+
+const links = [
+  {
+    href: "https://github.com/TheJolman/resume/blob/main/resume.pdf",
+    content: "PDF Version",
+  },
+];
 
 // TODO: Maybe implement some caching here
 export default function ResumePage({ data }: PageProps<string>) {
@@ -23,8 +31,13 @@ export default function ResumePage({ data }: PageProps<string>) {
       <Head>
         <style dangerouslySetInnerHTML={{ __html: CSS }} />
       </Head>
-      <main class="mx-5 my-5 markdown-body" dangerouslySetInnerHTML={{ __html: data }} />
+      <div class="mt-5">
+        <PortfolioLinks links={links} />
+        <main
+          class="mx-5 my-5 markdown-body"
+          dangerouslySetInnerHTML={{ __html: data }}
+        />
+      </div>
     </>
   );
 }
-
