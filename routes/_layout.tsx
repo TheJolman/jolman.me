6d21ed4b17c0018e81ea005b02621987dc8100b0
Josh Holman <josh@jolman.me>
2025-07-08 14:00:05 -0700

    deno fmt

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,5 +1,5 @@
-import { PageProps } from "$fresh/server.ts"
-import Navbar from "../components/Navbar.tsx"
+import { PageProps } from "$fresh/server.ts";
+import Navbar from "../components/Navbar.tsx";
 
 export default function Layout({ Component, state }: PageProps) {
   return (
